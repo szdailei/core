34cbe4eaa99c7fa206497c12d06516d7c55a55e0
Dailei <szdailei@gmail.com>
2021-11-16 18:06:46 +0800

    feat: support dev-scripts

--- scripts/index.js
+++
@@ -1,4 +0,0 @@
-import { buildServer, cleanServer } from './rollup/build-server.js';
-import { buildApp, cleanApp } from './rollup/build-app.js';
-
-export { buildServer, cleanServer, buildApp, cleanApp };
