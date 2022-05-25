bc0e8948c735327f8bf0a7f5cb540db8f6f62321
szdailei <szdailei@gmail.com>
2022-05-25 10:07:25 +0800

    update

--- client/styled/Span.jsx
+++ client/styled/Span.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// eslint-disable-next-line react/prop-types
 const Span = React.forwardRef(({ ...rest }, ref) => <span {...rest} ref={ref} />);
 
 export default Span;
